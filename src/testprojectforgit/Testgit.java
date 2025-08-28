18c3916a82dd5453a617b3c3b4b8c4a313876126
jesnans <jesnans@192.168.29.244>
2025-08-28 17:01:33 +0530

    My last commit

---
+++ src/testprojectforgit/Testgit.java
@@ -0,0 +1,17 @@
+package testprojectforgit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Stc first demo test by using main method");
+		System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.in");
+	}
+
+}
